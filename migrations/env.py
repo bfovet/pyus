28688962d04e054cf2af5c60119bed3e129c5b67
Benjamin Fovet <b.fovet@gmail.com>
2025-09-24 14:22:32 +0200

    feat: add an expires_at field for expiring URLs

--- migrations/env.py
+++ migrations/env.py
@@ -8,6 +8,7 @@ from alembic import context
 
 from pyus.config import settings
 from pyus.models import Model
+from pyus.models.url import ShortenedUrl
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Model.metadata
+target_metadata = ShortenedUrl.metadata
 
 config.set_main_option("sqlalchemy.url", settings.SQLITE_HOST)
 
--- src/pyus/url_shortening/schemas.py
+++ src/pyus/url_shortening/schemas.py
@@ -1,10 +1,14 @@
+from datetime import datetime
 from pydantic import UUID4, Field, HttpUrl, field_validator
 
 from pyus.kit.schemas import IDSchema, Schema, TimestampedSchema
 
 
 class ShortenedUrlBase(TimestampedSchema, IDSchema):
-    id: UUID4 = Field(description="The ID of the URL.", exclude=True)
+    expires_at: datetime | None = Field(
+        description="Expiration date of the URL."
+    )
+    id: UUID4 = Field(description="The ID of the URL.")
     original_url: HttpUrl = Field(description="Original URL.")
 
 
@@ -14,6 +18,9 @@ class ShortenedUrl(ShortenedUrlBase):
 
 class ShortenedUrlCreate(Schema):
     original_url: HttpUrl = Field(description="Original URL.")
+    expires_at: datetime | None = Field(
+        description="Expiration date of the URL."
+    )
 
     @field_validator("original_url", mode="after")
     @classmethod
